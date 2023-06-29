6569f39f6a5327a55fba605c0fbaaa9201bccb86
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-06-28 23:28:56 -0400

    Commit Inicial

--- src/dao/ProdutoDAO.ts
+++ src/dao/ProdutoDAO.ts
@@ -1,22 +1,8 @@
 import { Produto, ProdutoModel } from './../domains/ProdutoModel';
 
 export class ProdutoDAO {
-  constructor() {
-    this.produtoDAO = new ProdutoDAO()
-  }
   async save(Produto: Produto) {
-    const savedProduto = await ProdutoModel.create(req.body);
-
-    if(savedProduto.lenght == 0) {
-      const { id, descricao, perecivel} = req.body
-    }
-
-    const contact = new ProdutoModel({
-      id,
-      descricao,
-      perecivel
-    })
-    const savedProduto = await this.produtoDAO.save(Produto)
+    const savedProduto = await ProdutoModel.create(Produto);
     return savedProduto;
   }
 
