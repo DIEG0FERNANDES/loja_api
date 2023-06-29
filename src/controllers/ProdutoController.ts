67410dc932b256ce593cb634f97a7df12a5ec004
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-06-28 23:45:04 -0400

    Commit Inicial

--- src/controllers/ProdutoController.ts
+++ src/controllers/ProdutoController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { ProdutoDAO } from './../dao/ProdutoDAO';
+import { ProdutoModel } from '../domains/ProdutoModel';
 
 export class ProdutoController {
   private _produtoDAO: ProdutoDAO;
@@ -8,12 +9,16 @@ export class ProdutoController {
     this._produtoDAO = new ProdutoDAO();
   }
 
-  async save(req: Request, res: Response) {
-    res.json('produto');
+  async cadastroItem(req: Request, res: Response) {
+    const { descricao, perecivel } = req.body;
+    const produto = new ProdutoModel({ descricao, perecivel });
+    await this._produtoDAO.cadastroItem(produto);
+    res.status(201).send('Produto Cadastrado com sucesso')
   }
 
-  async delete(req: Request,res:Response){
-    await this._produtoDAO.delete();
+  async delete(req: Request, res: Response) {
+    const { id } = req.params;
+    await this._produtoDAO.delete(id);
     res.status(204).send('Produto Removido com sucesso')
   }
 }
