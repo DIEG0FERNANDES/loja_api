9d42e653d3bf19e344599a0dc9e703cca87c2430
Diego_dos_Santos_Fernandes <55159614+DIEG0FERNANDES@users.noreply.github.com>
2023-06-28 22:35:24 -0400

    Commit Inicial

--- src/domains/ProdutoModel.ts
+++ src/domains/ProdutoModel.ts
@@ -2,10 +2,16 @@ import { Document, Schema, model } from 'mongoose';
 
 export interface Produto extends Document {
   produto: string;
+  item: string;
 }
 
 const schema = new Schema<Produto>({
   produto: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  item: {
     type: String,
     required: true,
     unique: true
